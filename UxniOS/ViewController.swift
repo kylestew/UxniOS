382c091ccb0e62de51c706eacad8b18f761f51ad
Kyle Stewart <kylestew@gmail.com>
2021-07-13 13:40:51 -0600

    Create xcode project

---
+++ UxniOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UxniOS
+//
+//  Created by Kyle Stewart on 7/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
