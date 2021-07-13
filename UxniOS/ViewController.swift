0915deab6021987bf1cc9577fa517c1b786ffc0f
Kyle Stewart <kylestew@gmail.com>
2021-07-13 13:58:48 -0600

    Incoporate Uxn submodule and get C to compile along side Swift

--- UxniOS/ViewController.swift
+++ UxniOS/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  UxniOS
-//
-//  Created by Kyle Stewart on 7/13/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        bootuxn(nil)
     }
 
-
 }
-
